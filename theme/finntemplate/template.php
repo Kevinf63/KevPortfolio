dd3b216c73ea1c4111e6b7cce05dfaa08d2df5f5
Kevin Finnegan <kevin1.finnegan@gmail.com>
2015-06-28 13:16:39 +0100

    favicons and desktop icons updated.

--- theme/finntemplate/template.php
+++ theme/finntemplate/template.php
@@ -20,8 +20,27 @@ $themeurl = get_theme_url($echo);
     <head>
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"> 
-        <link rel="shortcut icon" href="<?php echo $siteurl ?>/favicon.ico?v=2" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!--FAVICON AND DESKTOP ICONS-->
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $siteurl ?>apple-touch-icon-57x57.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $siteurl ?>apple-touch-icon-60x60.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $siteurl ?>apple-touch-icon-72x72.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $siteurl ?>apple-touch-icon-76x76.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $siteurl ?>apple-touch-icon-114x114.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $siteurl ?>apple-touch-icon-120x120.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $siteurl ?>apple-touch-icon-144x144.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $siteurl ?>apple-touch-icon-152x152.png?v3=E6vx09KgLX">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $siteurl ?>apple-touch-icon-180x180.png?v3=E6vx09KgLX">
+        <link rel="icon" type="image/png" href="<?php echo $siteurl ?>favicon-32x32.png?v3=E6vx09KgLX" sizes="32x32">
+        <link rel="icon" type="image/png" href="<?php echo $siteurl ?>android-chrome-192x192.png?v3=E6vx09KgLX" sizes="192x192">
+        <link rel="icon" type="image/png" href="<?php echo $siteurl ?>favicon-96x96.png?v3=E6vx09KgLX" sizes="96x96">
+        <link rel="icon" type="image/png" href="<?php echo $siteurl ?>favicon-16x16.png?v3=E6vx09KgLX" sizes="16x16">
+        <link rel="manifest" href="<?php echo $siteurl ?>manifest.json?v3=E6vx09KgLX">
+        <link rel="shortcut icon" href="<?php echo $siteurl ?>favicon.ico?v3=E6vx09KgLX">
+        <meta name="msapplication-TileColor" content="#2ecc71">
+        <meta name="msapplication-TileImage" content="<?php echo $siteurl ?>mstile-144x144.png?v3=E6vx09KgLX">
+        <meta name="theme-color" content="#ffffff">
 
         <!--ADAPTIVE IMAGES RESIZER-->
         <script>document.cookie = 'resolution=' + Math.max(screen.width, screen.height) + '; path=/';</script>
